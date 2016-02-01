31eaa54c5a913df345f41c3bc4c213c99091ee58
Alex Bennett <alexandergraybennett@gmail.com>
2016-02-01 11:20:48 -0700

    :verticle_traffic_light:

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var unirest = require('unirest')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/unirest', function(req, res, next) {
+  
+  unirest.get('https://reddit.com/.json')
+
+  .end(function (response) {
+
+    res.json({ resposne: response});
+  });
+});
+
+module.exports = router;
